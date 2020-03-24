4f0b9dbf80911b6bc218ab3042184957639cc596
Aquatoriya <aquatoriya@bk.ru>
2020-03-24 19:42:39 +0300

    Added DB of stations; accidently changed DB, so now it doesn't cooperate with buttons AT ALL. I commented strings with the main body of "button's part" , but I have no idea how to make it work again.
    Also, MainActivity doesn't work. I don't know why.

--- app/src/main/java/m/verycool/metromap/Info.kt
+++ app/src/main/java/m/verycool/metromap/Info.kt
@@ -1,81 +1,61 @@
 package m.verycool.metromap
 
+
+import android.graphics.Typeface
 import android.os.Bundle
-import android.widget.ListView
-import android.widget.SimpleAdapter
+import android.text.Html
 import androidx.appcompat.app.AppCompatActivity
-import org.w3c.dom.Element
-import org.w3c.dom.Node
-import java.io.File
-import javax.xml.parsers.DocumentBuilderFactory
+import kotlinx.android.synthetic.main.info_activity.*
 
 
-class Info : AppCompatActivity() {
+class InfoActivity : AppCompatActivity() {
 
-    var empDataHashMap = HashMap<String, String>()
-    var empList: ArrayList<HashMap<String, String>> = ArrayList()
+    private lateinit var nameData: String
+    private lateinit var dateData: String
+    private lateinit var depthData: String
+    private lateinit var architectsData: String
+    private lateinit var historyData: String
+    private lateinit var images : IntArray
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.info_activity)
 
-
-        fun stationList(name: String) : Boolean {
-            val lv = findViewById<ListView>(R.id.listView)
-            val xmlFile = File("db_handler.xml")
-            val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile)
-            var list = doc.getElementsByTagName("station")
-            for (i in 0 until list.length) {
-                if (list.item(0).getNodeType().equals(Node.ELEMENT_NODE) ) {
-                    empList.clear()
-                    //creating instance of HashMap to put the data of node value
-                    empDataHashMap = HashMap()
-                    val element = list.item(i) as Element
-                    empDataHashMap.put("name", getNodeValue("name", element))
-                    empDataHashMap.put("date", getNodeValue("date", element))
-                    empDataHashMap.put("depth", getNodeValue("depth", element))
-                    empDataHashMap.put("architects", getNodeValue("architects", element))
-                    //adding the HashMap data to ArrayList
-                    empList.add(empDataHashMap)
-
-
-                    val adapter = SimpleAdapter(this@Info, empList, R.layout.info_activity, arrayOf("name", "date", "depth", "architects"), intArrayOf(R.id.name, R.id.date, R.id.depth, R.id.architects))
-                    lv.setAdapter(adapter)
-
-                }
-            }
-            return false
-        }
-
-        fun main(args: Array<String>) {
-            println(stationList("Автово"))
-        }
+        nameData = intent.getStringExtra("name")
+        dateData = intent.getStringExtra("date")
+        depthData = intent.getStringExtra("depth")
+        architectsData = intent.getStringExtra("architects")
+        historyData = intent.getStringExtra("hours")
 
 
 
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setTitle(nameData)
+
+
+        val typeface = Typeface.createFromAsset(assets, "font/Roboto-ThinItalic.ttf")
+        name.text = Html.fromHtml("<b>Название станции: </b> $nameData")
+        date.text = Html.fromHtml("<b>Станция была построена: </b> $dateData")
+        depth.text = Html.fromHtml("<b>Глубина станции: </b> $depthData")
+        architects.text = Html.fromHtml("<b>Архитекторы: </b> $architectsData")
+        history.text = Html.fromHtml("<b>Краткая информация о станции: </b> $historyData")
+
+        name.typeface = typeface
+        date.typeface = typeface
+        depth.typeface = typeface
+        architects.typeface = typeface
+        history.typeface = typeface
 
     }
 
-
-    // function to return node value
-    protected fun getNodeValue(tag: String, element: Element): String {
-        val nodeList = element.getElementsByTagName(tag)
-        val node = nodeList.item(0)
-        if (node != null) {
-            if (node.hasChildNodes()) {
-                val child = node.getFirstChild()
-                while (child != null) {
-                    if (child.getNodeType() === Node.TEXT_NODE) {
-                        return child.getNodeValue()
-                    }
-                }
-            }
-        }
-        return ""
-
-
-    }
+    //override fun onOptionsItemSelected(item: MenuItem): Boolean {
+      //  when (item.itemId) {
+        //    android.R.id.home -> {
+          //      this.finish()
+            //    return true
+           // }
+           // else -> return super.onOptionsItemSelected(item)
+        //}
+   // }
 }
-
-
-
---
+++ app/src/main/java/m/verycool/metromap/StationData.kt
@@ -0,0 +1,5 @@
+package m.verycool.metromap
+
+
+class StationData (var id : Int, var name: String, var date: String, var depth: String, var architects: String,
+                        var history:String, var images: List<Int>)
